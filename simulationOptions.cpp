993165efc275638414415973298354af866993d8
Ben <schueller@th.physik.uni-frankfurt.de>
2017-09-05 22:41:31 +0200

    simOpt, Corr3 wie bei Xu

--- simulationOptions.cpp
+++ simulationOptions.cpp
@@ -86,8 +86,8 @@ void SimulationOptions::setInitValues(){
 	//physical settings
 	this->k_b = 1; // k_boltzmann constant
 	this->temperature = 1.0; //2.0
-	this->mass =1.0/16.0*this->temperature; //1.0; //mass of particle
-	//this->D = 10; //note: shouldn't influence evolution related to correlation function 3
+	this->mass =1.0;//1.0/16.0*this->temperature; //1.0; //mass of particle
+	this->D = 4; //note: shouldn't influence evolution related to correlation function 3
         //this->tau = 5; // see paper /only important for first correlation function
 	//this->a = 7.6; // only important for second correlation function
 	//this->chi=3.5;//correlation time for third correlation function
@@ -95,12 +95,12 @@ void SimulationOptions::setInitValues(){
 
 	//statistical/program settings
  	this->t0 = 0.0; //time interval [t0, t1]
-        this->t1 = 50;     //1.0: für kb*T=0.5 Limes
+        this->t1 = 30;     //1.0: für kb*T=0.5 Limes
 	this->tSettling = 100.0; // time needed for I(t) to be approximately 0
 	this->timeSettled = (this->t1-this->t0)/3.0; //approximate time particles need to be in equilibrium - only important for kinetic Energy Average - not yet in external call
         this->nStepsFactor = 30;//round(this->t1-this->t0);
         this->nStepsTwo = 7;   //15: für kb*T=0.5 Limes
-	this->nSteps = 2000;//nStepsFactor*pow(2, this->nStepsTwo); // number of final datapos (stored), must be devidable by 2
+	this->nSteps = nStepsFactor*pow(2, this->nStepsTwo); ///2000; // number of final datapos (stored), must be devidable by 2
         //this->npTen=4;
         //this->npTwo=0;
 	this->np = pow(10,npTen)*pow(2,this->npTwo); //number of averaged simulations (number of particles)
@@ -159,8 +159,8 @@ void SimulationOptions::setInitValues(){
 // 	cout << "omegaB: " << sqrt(4*this->Ub/(this->mass*pow((this->xb-this->xc),2.0))) << endl;
 // 	cout << "m*omegaB: " << this->mass*pow(sqrt(4*this->Ub/(this->mass*pow((this->xb-this->xc),2.0))),2.0) << endl;
 	
-	this->D = 2*this->k_b*this->temperature*this->mass*sqrt(4*this->Ub/(this->mass*pow((this->xb-this->xc),2.0)));   //nur zum check des Skalierungsverhalten von Kramersrate, Wieder auskommentieren später
-	cout << "D: " << 2*this->k_b*this->temperature*this->mass*sqrt(4*this->Ub/(this->mass*pow((this->xb-this->xc),2.0))) << endl;
+// 	this->D = 2*this->k_b*this->temperature*this->mass*sqrt(4*this->Ub/(this->mass*pow((this->xb-this->xc),2.0)));   //nur zum check des Skalierungsverhalten von Kramersrate, Wieder auskommentieren später
+// 	cout << "D: " << 2*this->k_b*this->temperature*this->mass*sqrt(4*this->Ub/(this->mass*pow((this->xb-this->xc),2.0))) << endl;
 
 	//energy animation (ea) settings
 	this->eaBool = false; // energy Animation?
@@ -184,7 +184,7 @@ void SimulationOptions::setInitValues(){
     // wähle Anfangsbedingungen für Vergleich mit paper oder beliebig
     if(this->paperBool==0)
     {
-      x0=1.0*this->xc;
+      x0=0.0;//1.0*this->xc;
       v0=0.0;
     }
     else{ //Berechnung der Anfangsgeschwindigkeit über die entsprechenden Eigenwerte 
@@ -287,8 +287,8 @@ void SimulationOptions::setInitValues(){
 
 
 void SimulationOptions::setDependentVariables(){
-// this->dt = (t1-t0)/((double)nSteps);
-this->dt = (5*xc)/((double)nSteps); //mit passendem Skalierungsverhalten für Pot 7
+this->dt = (t1-t0)/((double)nSteps);
+// this->dt = (5*xc)/((double)nSteps); //mit passendem Skalierungsverhalten für Pot 7
 cout << "dt " << dt << " mass " << mass << endl;
 
   //----prepare simulations----
